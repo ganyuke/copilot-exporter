ae87587de4d2f6e43b1fe8326cab08d982a6fbed
ganyuke <personalemailforedits@gmail.com>
2025-07-25 22:25:37 -0700

    simplify button creation injector

--- src/button.ts
+++ src/button.ts
@@ -1,5 +1,30 @@
 import { APP_TAG } from "./main";
 
+async function hookIntoSidebar(callback: Function) {
+    const sidebarRoot = await waitForElement('[data-testid="appbar-v2"]'); // this should always be here
+
+    if (!sidebarRoot) {
+        const log = `${APP_TAG} Could not hook into sidebar root!`;
+        console.error(log);
+        throw new Error(log);
+    }
+
+    const observer = new MutationObserver(() => {
+        const allConversationsBtn = document.getElementById("all-history") as HTMLButtonElement;
+
+        if (allConversationsBtn && allConversationsBtn.dataset.hooked !== "1") {
+            allConversationsBtn.dataset.hooked = "1"
+            createExportButton(allConversationsBtn, callback);
+        }
+    });
+
+    observer.observe(sidebarRoot, {
+        childList: true,
+        subtree: true,
+    });
+
+}
+
 function waitForElement(selector: string, timeout = 10000): Promise<HTMLElement> {
     return new Promise((resolve, reject) => {
         const found = document.querySelector<HTMLElement>(selector);
@@ -40,29 +65,43 @@ function createExportButton(baseBtn: HTMLButtonElement, callback: Function) {
     });
 
     // I sure hope this still has a parent!
-    baseBtn.parentElement?.insertBefore(exportBtn, baseBtn.nextSibling);
+    baseBtn.parentElement?.append(exportBtn);
 
 }
+/*
+async function sidebarOpened(callback: Function) {
+    // the "Chat" section isn't loaded unless the sidebar is expanded
+    const chatBtn = document.getElementById("chat");
 
-export async function injectExportButton(callback: Function) {
+    // do nothing until the sidebar is opened
+    if (chatBtn) {
+        return;
+    }
+
     // this is the "All conversations" button
     const baseSelector = 'button#all-history';
 
     // this is the expand fold labeled "Conversations"
     // under the M365 sidebar "Chat" heading. It needs
     // to be expanded for "All conversations" to appear.
-    const conversationBtn = document.querySelector('button[aria-label="Conversations"]')
+    const conversationBtn = await waitForElement('button[aria-label="Conversations"]');
 
-    // we'll listen in for when "Conversations"
-    // is opened so we can inject the "Export" button.
-    if (conversationBtn) {
+    if (conversationBtn && !conversationBtn.dataset.hooked) {
+        conversationBtn.dataset.hooked = "true";
+
+        // we'll listen in for when "Conversations"
+        // is opened so we can inject the "Export" button.
         conversationBtn.addEventListener("click", () => {
             const allConversationsBtn = document.getElementById("all-history") as HTMLButtonElement;
             if (!allConversationsBtn) {
-                throw new Error(`${APP_TAG} Failed to find "All conversations" button.`)
+                throw new Error(`${APP_TAG} Failed to find "All conversations" button.`);
             }
             createExportButton(allConversationsBtn, callback);
-        })
+        });
+    }
+
+    if (conversationBtn) {
+        conversationBtn.addEventListener()
     } else {
         // If this doesn't exist... um. We'll just wait
         // until the "All conversations" button exists
@@ -74,4 +113,16 @@ export async function injectExportButton(callback: Function) {
             console.error(`${APP_TAG} Failed to insert export button with error: ${err}`);
         }
     }
+}
+
+export async function injectExportButton(callback: Function) {
+    const sidebarBtn = await waitForElement('button[aria-label="Expand side navigation panel"]') as HTMLButtonElement;
+    sidebarBtn.addEventListener('click', () => sidebarOpened(callback));
+
+    // in case it's already open
+    sidebarOpened(callback);
+}*/
+
+export function injectExportButton(callback: Function) {
+    hookIntoSidebar(callback);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Microsoft_365_%282022%29.svg',
+        namespace: "@ganyuke",
         match: ['https://m365.cloud.microsoft/', 'https://m365.cloud.microsoft/chat/*'],
         'run-at': 'document-end',
       },
